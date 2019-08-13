c706b7abc9c5dac14e90b008490591cb2a9f690c
Ryan Jasinski <jasinski.17@gmail.com>
2019-08-13 15:15:36 -0400

    intial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const chalk = require('chalk');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, './build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(
+    chalk.green('Express server listening on PORT: '),
+    chalk.cyan(PORT)
+  );
+});
